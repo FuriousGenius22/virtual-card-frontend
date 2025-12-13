55711a60a76b26bfb12eda4c8c7b643c26c2b95a
Furiousgenius22 <dongfangzhizhu123@outlook.com>
2025-12-13 18:56:16 +0200

    feat: Pages and components set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Accounts from "./pages/Accounts";
+import Cards from "./pages/Cards";
+import LandingPage from "./pages/LandingPage";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
     <div>
-      <div className="w-[100px] h-[50px] rounded-lg bg-blue-600 text-white">Initial Project</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
