b4c84ea298e1c94f0a508fa36a1d9d7abc0d1f55
Darkshirtboy22 <Andromeda0723@outlook.com>
2025-12-14 09:59:53 -0800

    test2

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,40 +1,31 @@
 import { Button, Card, CardBody, CardFooter, CardHeader, Chip, CircularProgress, Divider, Link } from '@nextui-org/react';
 import type {FC} from 'react';
+import React from 'react';
 
 interface Props {}
 
 const Dashboard: FC<Props> = () => {
-return <div>
-  <Button color="primary">Button</Button>
-  <Card className="w-[240px] h-[240px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500">
-      <CardBody className="items-center justify-center pb-0">
-        <CircularProgress
-          classNames={{
-            svg: "w-36 h-36 drop-shadow-md",
-            indicator: "stroke-white",
-            track: "stroke-white/10",
-            value: "text-3xl font-semibold text-white",
-          }}
-          showValueLabel
-          strokeWidth={4}
-          value={70}
-        />
-      </CardBody>
 
-      <CardFooter className="items-center justify-center pt-0">
-        <Chip
-          classNames={{
-            base: "border border-white/30",
-            content: "text-white/90 text-sm font-semibold",
-          }}
-          variant="bordered"
-        >
-          2800 Data points
-        </Chip>
-      </CardFooter>
-    </Card>
-</div>;
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => (v >= 100 ? 0 : v + 10));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <CircularProgress
+      aria-label="Loading..."
+      color="warning"
+      showValueLabel={true}
+      size="lg"
+      value={value}
+    />
+  )
 
 };
+//This is for git hub!
 
 export default Dashboard;
