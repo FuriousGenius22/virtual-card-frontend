82c294e2a93560ab00e113705b9a08432728cce2
Darkshirtboy22 <Andromeda0723@outlook.com>
2025-12-14 11:51:16 -0800

    Add a not found page!

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import Cards from "./pages/Cards";
 import LandingPage from "./pages/LandingPage";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/OtherPage/NotFound";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/cards" element={<Cards />} />
-          <Route path="/signin" element={<SignIn />} />
-          
-          <Route path="/dashboard" element = {<Dashboard/>}/>
+          {/* <Route element = {<AppLayout/>}> */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/cards" element={<Cards />} />
+            <Route path="/signin" element={<SignIn />} />
+            
+            <Route path="/dashboard" element = {<Dashboard/>}/>
+            <Route path = "*" element = {<NotFound/>}/>
+          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </div>
