b1f267f1aee0fafc7843afcd48a7e2f7733783a6
FuriousGenius22 <dongfangzhizhu123@outlook.com>
2025-12-29 07:58:08 -0800

    tyuio

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { Button, Image } from '@nextui-org/react';
 import type { FC } from 'react';
-import React from 'react';
+
 import PageMeta from '../components/common/PageMeta';
 import AccountCard from '../components/common/AccountCard';
 import { Link } from 'react-router-dom';
